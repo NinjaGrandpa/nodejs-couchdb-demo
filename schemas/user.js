c0d2249c25e325076f9977364887507f74eb7fca
NinjaGrandpa <max@jenslov.se>
2023-06-26 16:17:25 +0200

    updated 'user' schema to send '_rev' and '_id'

--- schemas/user.js
+++ schemas/user.js
@@ -1,6 +1,8 @@
 const Joi = require("joi");
 
 module.export = Joi.object().keys({
+    _rev: Joi.string(),
+    _id: Joi.string(),
     email: Joi.string().email().required(),
     username: Joi.string().alphanum().min(3).max(30).required(),
     password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/),
