ddbd5b2ee57f7425ba48eb2bfa8b75d6174a1800
NinjaGrandpa <max@jenslov.se>
2023-06-26 16:00:51 +0200

    Updated the 'users' schema to use the new validation functions

--- db/users.js
+++ db/users.js
@@ -1,5 +1,9 @@
+const schemas = require("../schemas");
+
 const users = require("../couchdb").use("users");
 
-exports.create = function create(user, cb) {
+exports.create = schemas.validating("user", createUser);
+
+function createUser(user, cb) {
     users.insert(user, user.email, cb);
 };
