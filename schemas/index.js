920644f7c52ff5fc7edec85c30908af1a1b397df
NinjaGrandpa <max@jenslov.se>
2023-06-26 15:58:46 +0200

    Created functions in module 'index.js' to validate schemas

---
+++ schemas/index.js
@@ -0,0 +1,37 @@
+let schemaNames = ['user'];
+let schemas = {};
+
+schemaNames.forEach(function (schemaName) {
+    schemas[schemaName] = require("./" + schemaName);
+});
+
+exports.validate = validate;
+
+function validate(doc, schema, cb) {
+    if (typeof schema == "string") {
+        schema = schemas[schema];
+    }
+
+    if (!schema) {
+        cb(new Error("Unknow schema"));
+    } else {
+        Joi.validate(doc, schema, cb);
+    }
+};
+
+exports.validating = function validating(schemaName, fn) {
+    let schema = schemas[schemaName];
+    if (!schema) {
+        throw new Error("Unknow schema: " + schemaName);
+    }
+
+    return function (doc, cb) {
+        validate(doc, schema, function (err, doc) {
+            if (err) {
+                cb(err)
+            } else {
+                fn.call(null, doc, cb);
+            }
+        });
+    };
+};
