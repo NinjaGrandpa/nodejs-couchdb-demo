383b2913e7a6658b45b80cb84f0ba5f09ff13e76
NinjaGrandpa <max@jenslov.se>
2023-06-26 16:17:48 +0200

    created function to update users

--- db/users.js
+++ db/users.js
@@ -3,8 +3,18 @@ const errors = require("../errors");
 
 const users = require("../couchdb").use("users");
 
+// Create user
 exports.create = schemas.validating("user", createUser);
 
 function createUser(user, cb) {
     users.insert(user, user.email, errors.wrapNano(cb));
 };
+
+// Update user
+
+exports.update = schemas.validating("user", updateUser);
+
+function updateUser(user, cb) {
+    users.insert(user, errors.wrapNano(cb));
+
+}
