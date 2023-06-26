ff94e11265709795b7e45f69690cf468cb21cbed
NinjaGrandpa <max@jenslov.se>
2023-06-26 15:50:26 +0200

    added schema 'user.js to validate correct data when inserting to database'

---
+++ schemas/user.js
@@ -0,0 +1,16 @@
+const Joi = require("joi");
+
+module.export = Joi.object().keys({
+    email: Joi.string().email().required(),
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/),
+    access_token: [Joi.string(), Joi.number()],
+    birthyear: Joi
+        .number()
+        .integer()
+        .min(1900)
+        .max(
+            (new Date())
+                .getFullYear()
+        ),
+});
