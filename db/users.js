1a3386b4487fff9c4d9e365f3251cf16194ca6a1
NinjaGrandpa <max@jenslov.se>
2023-06-27 12:07:34 +0200

    Created function 'updateUserDiff()' in 'users.js'

--- db/users.js
+++ db/users.js
@@ -11,9 +11,41 @@ function createUser(user, cb) {
 };
 
 // Update user
-
 exports.update = schemas.validating("user", updateUser);
 
 function updateUser(user, cb) {
     users.insert(user, errors.wrapNano(cb));
+}
+
+
+// Update user diff.
+exports.updateDiff = updateUserDiff;
+
+function updateUserDiff(userDiff, cb) {
+    merge();
+
+    function merge() {
+        users.get(userDiff._id, errors.wrapNano(function (err, user) {
+            if (err) {
+                cb(err);
+            } else {
+                extend(user, userDiff);
+                schemas.validate(user, 'user', function (err) {
+                    if (err) {
+                        cb(err);
+                    } else {
+                        users.insert(user, errors.wrapNano(done));
+                    }
+                })
+            }
+        }));
+
+        function done(err) {
+            if (err && err.statusCode == 409 && !userDiff._rev) {
+                merge(); // try again
+            } else {
+                cb.apply(null, arguments);
+            }
+        }
+    }
 }
